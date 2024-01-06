601d335b6bd7b1f2d5f85926c92a6a49c0e0d01f
Ivan Kisyov <dev.vanks@gmail.com>
2024-01-06 18:16:24 +0200

    Add delete button to blog post items

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   // step 4
@@ -13,12 +14,34 @@ const IndexScreen = () => {
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather name="trash" style={styles.icon} />
+            </View>
+          );
+        }}
       />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray",
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;
